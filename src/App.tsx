2701a14774b90f52db6d795e46e0b59a84076cea
lucastenani <lucas.tenani3@gmail.com>
2023-11-28 16:51:49 -0300

    Creating transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "styled-components";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
+import { Transactions } from "./pages/Transactions";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
-import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
       <GlobalStyle />
     </ThemeProvider>
   );
